e20d1d52d22be158d76e7a88c4908661f8331e39
TBone2448777 <taylorsloan3430@gmail.com>
2023-12-04 14:18:12 -0600

    Khania

--- script.js
+++ script.js
@@ -35,9 +35,9 @@ window.addEventListener("DOMContentLoaded", function () {
             </div>
         `);
     }
-    imageArrayN=["alex",      "cris",     "grant",  "alyssa",    "justin", "emely",          "kea", "serenity", "bishop",  "raven", "jeremiah", "jansen",    "lexi",   "trevor", "wubi",    "daniel", "seb",      "lauren",    "emma",   "max",        "ewa",       "mccleary", "clayton",   "justin2"]
-    fNameArrayN=["Alexander", "Cristian", "Grant",  "Alyssa",    "Justin", "Emely",          "Kea", "Serenity", "Bishop",  "Raven", "Jeremiah", "Jansen",    "Alexia", "Trevor", "Wubi",    "Daniel", "Seb",      "Lauren",    "Emma",   "Maximilian", "Ewa",       "McCleary", "Clayton",   "Justin"]
-    lNameArrayN=["Miles",     "Cuevas",   "Booker", "Freimanis", "Kroh",   "Solis Aparicio", "Yeh", "Sosa",     "Markham", "Haney", "Jones",    "Breshears", "Bowlen", "Blair",  "Halefom", "Gaitan", "Gonzalez", "Fergerson", "Eubank", "Johnson",    "Samberger", "Stark",    "Robertson", "Basler"]
+    imageArrayN=["alex",      "cris",     "grant",  "alyssa",    "justin", "emely",          "kea", "serenity", "bishop",  "raven", "jeremiah", "jansen",    "lexi",   "trevor", "wubi",    "daniel", "seb",      "lauren",    "emma",   "max",        "ewa",       "mccleary", "clayton",   "justin2", "khania"]
+    fNameArrayN=["Alexander", "Cristian", "Grant",  "Alyssa",    "Justin", "Emely",          "Kea", "Serenity", "Bishop",  "Raven", "Jeremiah", "Jansen",    "Alexia", "Trevor", "Wubi",    "Daniel", "Seb",      "Lauren",    "Emma",   "Maximilian", "Ewa",       "McCleary", "Clayton",   "Justin",  "Khania"]
+    lNameArrayN=["Miles",     "Cuevas",   "Booker", "Freimanis", "Kroh",   "Solis Aparicio", "Yeh", "Sosa",     "Markham", "Haney", "Jones",    "Breshears", "Bowlen", "Blair",  "Halefom", "Gaitan", "Gonzalez", "Fergerson", "Eubank", "Johnson",    "Samberger", "Stark",    "Robertson", "Basler",  "Clark"]
     peopleSection = Array.from(document.getElementsByClassName("pfpContainer"))[0]
     for (let i = 0; i<imageArrayN.length; i++){
         peopleSection.insertAdjacentHTML("beforeend", `
